885959f2461e1129b64a7dcee211b3a4d69e9840
Egor <egor_chek@mail.ru>
2023-08-29 13:28:35 +0400

    добавлена лента ячеек, отображение лайков и даты, с отображение изображения из мок данных

--- ImageFeedYandexPracticum/ImagesList/ImagesListCell.swift
+++ ImageFeedYandexPracticum/ImagesList/ImagesListCell.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
-    
     static let reuseIdentifier = "ImagesListCell"
-    
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var cellImage: UIImageView!
 }
