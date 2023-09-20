63f596ff7f17e825a391cab887e78fd3424c0481
Egor <egor_chek@mail.ru>
2023-09-20 20:59:23 +0400

    добавленно детальное окно изображения и сегвей к нему

---
+++ ImageFeedYandexPracticum/Profile/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  ImageFeedYandexPracticum
+//
+//  Created by admin on 14.09.2023.
+//
+
+import Foundation
---
+++ ImageFeedYandexPracticum/SingleImageViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeedYandexPracticum
+//
+//  Created by admin on 20.09.2023.
+//
+
+import Foundation
