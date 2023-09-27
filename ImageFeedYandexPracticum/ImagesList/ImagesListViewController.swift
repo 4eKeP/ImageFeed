24e273ae08539dba63a2ff4d3765ff3ad1dbf2be
Egor <egor_chek@mail.ru>
2023-09-27 20:11:10 +0400

    исправленна ошибка в названии showSingleImageIdentifier

--- ImageFeedYandexPracticum/ImagesList/ImagesListViewController.swift
+++ ImageFeedYandexPracticum/ImagesList/ImagesListViewController.swift
@@ -11,7 +11,7 @@ class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
-    private let ShowSingleImageSegueIdentifier = "ShowSingleImage"
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
     
     
     override func viewWillAppear(_ animated: Bool) {
@@ -62,7 +62,7 @@ extension ImagesListViewController: UITableViewDataSource {
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        performSegue(withIdentifier: ShowSingleImageSegueIdentifier, sender: indexPath)
+        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
         
     }
     
@@ -96,7 +96,7 @@ extension ImagesListViewController {
 
 extension ImagesListViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == ShowSingleImageSegueIdentifier {
+        if segue.identifier == showSingleImageSegueIdentifier {
             let viewController = segue.destination as! SingleImageViewController
             let indexPath = sender as! IndexPath
             let image = UIImage(named: photosName[indexPath.row])
