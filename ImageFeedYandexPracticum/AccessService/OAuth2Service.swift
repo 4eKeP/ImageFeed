23098b9d2be0cfd4ab2583a363192d93c1f963db
Egor <egor_chek@mail.ru>
2023-10-15 15:18:34 +0400

    исправленна ситуация гонки в запросе ключа аутентификации

--- ImageFeedYandexPracticum/AccessService/OAuth2Service.swift
+++ ImageFeedYandexPracticum/AccessService/OAuth2Service.swift
@@ -8,9 +8,12 @@
 import Foundation
 
 final class OAuth2Service {
-    private let oauth2TokenStorage = OAuth2TokenStorage()
     static let shared = OAuth2Service()
+    private let oauth2TokenStorage = OAuth2TokenStorage()
     private let urlSession = URLSession.shared
+    private var task: URLSessionTask?
+    private var lastCode: String?
+    
     private var authToken: String? {
         get {
             return OAuth2TokenStorage().token
@@ -19,21 +22,33 @@ final class OAuth2Service {
             OAuth2TokenStorage().token = newValue
         }
     }
+    
     func fetchOAuthToken(
         _ code: String,
         completion: @escaping (Result<String, Error>) -> Void ){
+            
+            assert(Thread.isMainThread)
+            if lastCode == code { return }
+            task?.cancel()
+            lastCode = code
+            
             let request = authTokenRequest(code: code)
             let task = object(for: request) { [weak self] result in
-                guard let self = self else { return }
-                switch result {
-                case .success(let body):
-                    let authToken = body.accessToken
-                    self.authToken = authToken
-                    completion(.success(authToken))
-                case .failure(let error):
-                    completion(.failure(error))
+                DispatchQueue.main.async {
+                    guard let self = self else { return }
+                    switch result {
+                    case .success(let body):
+                        let authToken = body.accessToken
+                        self.authToken = authToken
+                        completion(.success(authToken))
+                        self.task = nil
+                    case .failure(let error):
+                        self.lastCode = nil
+                        completion(.failure(error))
+                    }
                 }
             }
+            self.task = task
             task.resume()
         }
 }
--- ImageFeedYandexPracticum/SplashView/SplashViewController.swift
+++ ImageFeedYandexPracticum/SplashView/SplashViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ProgressHUD
 
 final class SplashViewController: UIViewController {
     private let authenticationSegueIdentifier = "ShowAuthenticationScreen"
@@ -30,7 +31,6 @@ final class SplashViewController: UIViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle {
         .lightContent
     }
-    //UIApplication.shared.windows.first
     private func switchToTabBarViewController() {
         let scenes = UIApplication.shared.connectedScenes
         let windowScenes = scenes.first as? UIWindowScene
@@ -57,6 +57,7 @@ extension SplashViewController {
 
 extension SplashViewController: AuthViewControllerDelegate {
     func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String) {
+        ProgressHUD.show()
         dismiss(animated: true) { [weak self] in
             guard let self = self else { return }
             self.fetchOAuthToken(code)
@@ -68,11 +69,13 @@ extension SplashViewController: AuthViewControllerDelegate {
             switch result {
             case .success:
                 self.switchToTabBarViewController()
+                ProgressHUD.dismiss()
             case .failure:
+                ProgressHUD.dismiss()
+                //TODO показать ошибку
                 break
             }
         }
     }
-    
 }
 
